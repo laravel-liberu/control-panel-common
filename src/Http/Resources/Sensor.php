8d0bf678e50ecd037897fe11f05c8dd734b5a92e
Curtis Delicata <curtis.delicata@outlook.com>
2023-10-13 13:51:37 +0000

    Fix sed typo

--- src/Http/Resources/Sensor.php
+++ src/Http/Resources/Sensor.php
@@ -4,7 +4,7 @@ namespace LaravelLiberu\ControlPanelCommon\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class Sliberur extends JsonResource
+class Sensor extends JsonResource
 {
     public function toArray($request)
     {
